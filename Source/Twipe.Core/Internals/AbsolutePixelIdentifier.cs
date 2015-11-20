77d51414eb569e56854fd58bf8ebdd6721a82efc
Mohammad Riazat <moriazat@gmail.com>
2015-11-20 13:25:30 -0500

    Improved the performance of the  conversion process a little bit as was mentioned in #1.

--- Source/Twipe.Core/Internals/AbsolutePixelIdentifier.cs
+++ Source/Twipe.Core/Internals/AbsolutePixelIdentifier.cs
@@ -1,23 +1,48 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Twipe.Core.Internals
 {
     public class AbsolutePixelIdentifier : PixelIdentifierBase
     {
+        private unsafe byte* CurrentBytePointer { get; set; }
+
         public override void IdentifyPixels()
         {
-            Color pixel;
+            BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
 
-            for (int x = 0; x < image.Width; x++)
+            unsafe
             {
+                CurrentBytePointer = (byte*)imageData.Scan0;
+
                 for (int y = 0; y < image.Height; y++)
                 {
-                    pixel = image.GetPixel(x, y);
-                    if (pixel.ToArgb() == Color.White.ToArgb())
-                        whites++;
-                    else if (pixel.ToArgb() == Color.Black.ToArgb())
-                        blacks++;
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        if (IsCurrentPixelWhite())
+                            whites++;
+                        else
+                            blacks++;
+
+                        CurrentBytePointer += 4;
+                    }
                 }
+            }
+
+            image.UnlockBits(imageData);
+        }
+
+        private bool IsCurrentPixelWhite()
+        {
+            unsafe
+            {
+                byte* ptr = CurrentBytePointer;
+
+                if ((*ptr == 255) && (*(ptr + 1) == 255) && (*(ptr + 2) == 255))
+                    return true;
+                else
+                    return false;
             }
         }
     }
--- Source/Twipe.Core/Internals/CharacterBitmapGenerator.cs
+++ Source/Twipe.Core/Internals/CharacterBitmapGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 
 namespace Twipe.Core.Internals
@@ -6,10 +8,17 @@ namespace Twipe.Core.Internals
     public class CharacterBitmapGenerator : BitmapGeneratorBase<Character>
     {
         private Task worker;
+        private BitmapData resultData;
+        private int bytesPerPixel;
+        private int heightInPixels;
+        private int widthInBytes;
+        private Bitmap[,] charImages;
+
+        private unsafe byte* FirstPixelPointer { get; set; }
 
         public CharacterBitmapGenerator(ITiledImage<Character> input) : base(input)
         {
-            // do nothing!
+            charImages = new Bitmap[input.Columns, input.Rows];
         }
 
         public override async Task<Bitmap> GenerateImageAsync()
@@ -23,19 +32,61 @@ namespace Twipe.Core.Internals
 
         private void GenerateImage()
         {
-            for (int row = 0; row < input.Rows; row++)
+            unsafe
             {
-                for (int column = 0; column < input.Columns; column++)
-                {
-                    using (Bitmap image = GetImageFor(input.GetTile(column, row)))
-                    {
-                        int startingX = column * input.TileSize;
-                        int startingY = row * input.TileSize;
+                resultData = result.LockBits(new Rectangle(0, 0, result.Width, result.Height), ImageLockMode.WriteOnly, result.PixelFormat);
 
-                        DrawImage(image, startingX, startingY);
+                bytesPerPixel = Bitmap.GetPixelFormatSize(result.PixelFormat) / 8;
+                heightInPixels = resultData.Height;
+                widthInBytes = resultData.Width * bytesPerPixel;
+                FirstPixelPointer = (byte*)resultData.Scan0;
+
+                Bitmap image = null;
+
+                //CreateImages(0);
+
+                for (int row = 0; row < input.Rows; row++)
+                {
+                    //if (row + 1 < input.Rows)
+                    //    CreateImagesAsync(row + 1);
+
+                    for (int column = 0; column < input.Columns; column++)
+                    {
+                        //image = charImages[column, row];
+                        using (image = GetImageFor(input.GetTile(column, row)))
+                        {
+                            int startingX = column * input.TileSize;
+                            int startingY = row * input.TileSize;
+
+                            DrawImage(image, startingX, startingY);
+
+                            //image.Dispose();
+                            //charImages[column] = null;
+                        }
                     }
                 }
+
+                result.UnlockBits(resultData);
             }
+        }
+
+        private async void CreateImagesAsync(int row)
+        {
+            for (int column = 0; column < input.Columns; column++)
+                charImages[column, row] = await GetImageForAsync(input.GetTile(column, row));
+        }
+
+        private void CreateImages(int row)
+        {
+            for (int column = 0; column < input.Columns; column++)
+                charImages[column, row] = GetImageFor(input.GetTile(column, row));
+        }
+
+        private async Task<Bitmap> GetImageForAsync(Character c)
+        {
+            Task<Bitmap> task = Task.Run(() => GetImageFor(c));
+
+            return await task;
         }
 
         private Bitmap GetImageFor(Character c)
@@ -54,16 +105,42 @@ namespace Twipe.Core.Internals
 
         private void DrawImage(Bitmap image, int startingX, int startingY)
         {
-            int tileSize = input.TileSize;
-            Color pixel;
-
-            for (int x = 0; x < tileSize; x++)
+            unsafe
             {
+                int tileSize = input.TileSize;
+                //Color pixel;
+                int positionInBytes;
+
+                BitmapData tileData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height),
+                                                     ImageLockMode.ReadOnly, image.PixelFormat);
+                byte* ptrPixelData = (byte*)tileData.Scan0;
+
                 for (int y = 0; y < tileSize; y++)
                 {
-                    pixel = image.GetPixel(x, y);
-                    result.SetPixel(startingX + x, startingY + y, pixel);
+                    byte* currentLine = FirstPixelPointer + ((startingY + y) * resultData.Stride);
+                    currentLine += startingX * bytesPerPixel;
+
+                    for (int x = 0; x < tileSize; x++)
+                    {
+                        //pixel = image.GetPixel(x, y);
+
+                        positionInBytes = (x * bytesPerPixel);
+
+                        currentLine[positionInBytes] = *ptrPixelData;
+                        currentLine[positionInBytes + 1] = *(ptrPixelData + 1);
+                        currentLine[positionInBytes + 2] = *(ptrPixelData + 2);
+                        currentLine[positionInBytes + 3] = *(ptrPixelData + 3);
+
+                        ptrPixelData += 4;
+
+                        //currentLine[positionInBytes] = (byte)pixel.B;
+                        //currentLine[positionInBytes + 1] = (byte)pixel.G;
+                        //currentLine[positionInBytes + 2] = (byte)pixel.R;
+                        //currentLine[positionInBytes + 3] = (byte)pixel.A;
+                    }
                 }
+
+                image.UnlockBits(tileData);
             }
         }
     }
